900831f29a0bf409cbfe15683e46a2b3554f6b06
Charisse Chow <charisse.chow@gmail.com>
2024-09-25 16:16:18 -0700

    Update udacity_chapter_4.sql

--- udacity_chapter_4.sql
+++ udacity_chapter_4.sql
@@ -9,3 +9,14 @@ SUBQUERES & TEMPORARY TABLES
   GROUP BY 1, 2
   ORDER BY 3 DESC) events_per_channel
 
+-- can be used in place of a table name, column name, or an individual value
+-- esp. conditional logic within CASE statement: with WHERE, JOIN, WHEN, SELECT
+  -- most conditioal logic only works with subqueries containing 1-cell results
+  -- except "IN" function works when inner query contains multiple results
+  -- DONT include alias b/c it is treated as an individual value (or set of values in IN case) rather than a table
+
+
+
+
+
+
