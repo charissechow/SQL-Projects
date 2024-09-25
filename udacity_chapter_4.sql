e8f08c2f363b6ceae041701a0ce2da74d3d7dead
Charisse Chow <charisse.chow@gmail.com>
2024-09-25 14:10:37 -0700

    Update udacity_chapter_4.sql

--- udacity_chapter_4.sql
+++ udacity_chapter_4.sql
@@ -1,2 +1,11 @@
 SUBQUERES & TEMPORARY TABLES
 
+-- original subquery goes in FROM statement
+-- * is used in the SELECT statement to pull all data from original query
+-- MUST use alias for table you nest within outer query
+  SELECT *
+  FROM (SELECT channel, DATE_TRUNC('day', occurred_at), COUNT(*) events
+  FROM web_events w
+  GROUP BY 1, 2
+  ORDER BY 3 DESC) events_per_channel
+
