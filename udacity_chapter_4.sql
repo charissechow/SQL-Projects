36e7fdc7c1c7508e93f7051c3e94ce8dd8a8e071
Charisse Chow <charisse.chow@gmail.com>
2024-09-25 17:32:05 -0700

    Update udacity_chapter_4.sql

--- udacity_chapter_4.sql
+++ udacity_chapter_4.sql
@@ -15,8 +15,59 @@ SUBQUERES & TEMPORARY TABLES
   -- except "IN" function works when inner query contains multiple results
   -- DONT include alias b/c it is treated as an individual value (or set of values in IN case) rather than a table
 
+1. Here is the necessary quiz to pull the first month/year combo from the orders table.
+SELECT DATE_TRUNC('month', MIN(occurred_at)) 
+FROM orders;
+
+2.Use result to find only orders that took place in the same month and year as the first order, and pull the average for each type of paper qty in this month
+SELECT AVG(standard_qty)stan_qty, AVG(gloss_qty) gloss_qty, AVG(poster_qty) poster_qty
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = (SELECT DATE_TRUNC('month', MIN(occurred_at))
+      FROM orders)
+  -- don't forget alias after each field (aka stan_qty) because it will all populate the same average for all columns
+
+QUIZ (SUBQUERIES)
+
+1. Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
+SELECT s.name sales_rep, r.name region_name, MAX(total_amt_usd)
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY sales_rep, region_name
+ORDER BY MAX(total_amt_usd) DESC
+LIMIT 1
+
+  Dawna Agnew
+
+2. For the region with the largest (sum) of sales total_amt_usd, how many total (count) orders were placed?
+SELECT s.name sales_rep, r.name region_name, o.total total_orders, SUM(total_amt_usd)
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY sales_rep, region_name, total_orders
+ORDER BY MAX(total_amt_usd) DESC
+LIMIT 1
+
+  28799 orders
+
+3. How many accounts had more total purchases than the account name which has bought the most standard_qty paper throughout their lifetime as a customer?
 
 
+4. For the customer that spent the most (in total over their lifetime as a customer) total_amt_usd, how many web_events did they have for each channel?
+
+
+5. What is the lifetime average amount spent in terms of total_amt_usd for the top 10 total spending accounts?
+
+
+6. What is the lifetime average amount spent in terms of total_amt_usd, including only the companies that spent more per order, on average, than the average of all orders.
 
 
 
