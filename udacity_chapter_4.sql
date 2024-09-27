c32cbfde7771c9f041271e7920b820cb5eea40da
Charisse Chow <charisse.chow@gmail.com>
2024-09-26 19:35:47 -0700

    Update udacity_chapter_4.sql

--- udacity_chapter_4.sql
+++ udacity_chapter_4.sql
@@ -29,7 +29,9 @@ WHERE DATE_TRUNC('month', occurred_at) = (SELECT DATE_TRUNC('month', MIN(occurre
 QUIZ (SUBQUERIES)
 
 1. Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
-SELECT s.name sales_rep, r.name region_name, SUM(total_amt_usd)
+SELECT T3.region_name as region_name, max_total_by_region, sales_rep
+FROM (SELECT region_name, MAX(total_amt_usd) max_total_by_region
+FROM (SELECT s.name sales_rep, r.name region_name, SUM(total_amt_usd) total_amt_usd
 FROM region r
 JOIN sales_reps s
 ON r.id = s.region_id
@@ -38,13 +40,9 @@ ON s.id = a.sales_rep_id
 JOIN orders o
 ON a.id = o.account_id
 GROUP BY sales_rep, region_name
-ORDER BY SUM(total_amt_usd) DESC
-LIMIT 1
-
-  Dawna Agnew
-
-2. For the region with the largest (sum) of sales total_amt_usd, how many total (count) orders were placed?
-SELECT r.name region_name, o.total total_orders, SUM(total_amt_usd)
+ORDER BY SUM(total_amt_usd) DESC) T1
+GROUP BY region_name) T2
+LEFT JOIN (SELECT s.name sales_rep, r.name region_name, SUM(total_amt_usd) total_amt_usd
 FROM region r
 JOIN sales_reps s
 ON r.id = s.region_id
@@ -52,14 +50,41 @@ JOIN accounts a
 ON s.id = a.sales_rep_id
 JOIN orders o
 ON a.id = o.account_id
-GROUP BY region_name, total_orders
-ORDER BY MAX(total_amt_usd) DESC
+GROUP BY sales_rep, region_name
+ORDER BY SUM(total_amt_usd) DESC) T3
+ON T3.region_name = T2.region_name
+AND T3.total_amt_usd = T2.max_total_by_region
+
+2. For the region with the largest (sum) of sales total_amt_usd, how many total (count) orders were placed?
+SELECT r.name, SUM(total_amt_usd) total_sales_by_region, COUNT(o.id) total_orders_by_region
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY r.name
+ORDER BY 2 DESC
 LIMIT 1
 
-  28799 orders
+  2357 orders
 
 3. How many accounts had more total purchases than the account name which has bought the most standard_qty paper throughout their lifetime as a customer?
+SELECT COUNT(a.id)
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id
+HAVING SUM(total)>(SELECT total_purchases_per_account
+FROM (SELECT a.name account_name, SUM(standard_qty) standard_orders, COUNT(o.total) total_purchases_per_account
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY account_name
+ORDER BY 2 DESC
+LIMIT 1) T1)
 
+  6912
 
 4. For the customer that spent the most (in total over their lifetime as a customer) total_amt_usd, how many web_events did they have for each channel?
 
