799295c54184bc7ef0d6cae2cfe7ac578e23ac4f
Charisse Chow <charisse.chow@gmail.com>
2024-09-30 19:10:32 -0700

    Update udacity_chapter_5.sql

--- udacity_chapter_5.sql
+++ udacity_chapter_5.sql
@@ -1,4 +1,4 @@
-DATA CLEANING
+DATA CLEANING (LEFT/RIGHT, POSITION/STRPOS, CONCAT, CAST, COALESCE)
 
 -- used as a field under SELECT to filter out what you want in a new column
 1. LEFT (e.g. LEFT(phone_number, 3)
@@ -23,7 +23,39 @@ GROUP BY 1
 ORDER BY 1
 
 3. Use the accounts table and a CASE statement to create two groups: one group of company names that start with a number and a second group of those company names that start with a letter. What proportion of company names start with a letter?
-
+SELECT 
+  COUNT(num) AS nums, 
+  COUNT(letter) AS letters
+FROM (SELECT name,
+CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') THEN 1 ELSE 0 END AS num,
+CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') THEN 0 ELSE 1 END AS letter
+FROM accounts
+GROUP BY 1)
 
 4. Consider vowels as a, e, i, o, and u. What proportion of company names start with a vowel, and what percent start with anything else?
+SELECT name,
+CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U') THEN 1 ELSE 0 END AS vowel,
+CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U') THEN 0 ELSE 1 END AS constanant
+FROM accounts
 
+POSITION
+  -- provides position of a string counting from left
+  -- (e.g. ',' IN city_state) AS comma_position
+STRPOS (String Position)
+  --provides position of a string couning from the left
+  -- (city_state, ',') AS substr_comma_position
+
+  -- EX. LEFT (city_state, POSITION(',' IN city_state)-1) AS city
+
+LOWER and UPPER -- lowercase or capitalize all characters of a string
+
+1. Use the accounts table to create first and last name columns that hold the first and last names for the primary_poc.
+SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') -1 ) first_name, 
+   RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name
+FROM accounts;
+
+CONCAT
+-- combine columns together across rows
+-- (e.g. CONCAT(firs_name, ' ', last_name)
+
+  
